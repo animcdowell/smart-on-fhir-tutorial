ae3e399947c53df4712e9da836983ea6b5b8a140
Anita McDowell <anita@Anitas-MacBook-Pro.local>
2019-10-19 15:31:17 -0700

    Fixed js file

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -37,10 +37,12 @@
           }
 
           var height = byCodes('8302-2');
+          var weight = byCodes('3141-9');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -80,6 +82,7 @@
       gender: {value: ''},
       birthdate: {value: ''},
       height: {value: ''},
+      weight: {value: ''},
       systolicbp: {value: ''},
       diastolicbp: {value: ''},
       ldl: {value: ''},
@@ -123,6 +126,7 @@
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
     $('#height').html(p.height);
+    $('#weight').html(p.weight);
     $('#systolicbp').html(p.systolicbp);
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
